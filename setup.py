1fd5b73fdb17ddb5fe3212cfc97d2fbce1326900
Tom Adams <tom@holizz.com>
2010-11-24 16:11:41 -0500

    Version bump to 0.2.2

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import distutils.core
+import os
 
 distutils.core.setup(
         name = 'cali',
-        version = open('VERSION').read().strip(),
+        version = open(os.path.join(os.path.dirname(__file__),'VERSION')).read().strip(),
         description = 'Interactive cal(1) (but the name ical was taken)',
         author = 'Tom Adams',
         author_email = 'tom@holizz.com',
